9496a6e2c8264c4dfb6222a50488a166ff62d715
lwmacct <lwmacct@icloud.com>
2025-05-08 01:52:42 +0800

    -

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lwmacct/250300-go-mod-mkafka/pkg/mkafka"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	config := &mkafka.Config{}
+	_ = config
 }
